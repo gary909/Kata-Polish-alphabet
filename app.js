25c81deda76a2660f63efbd47dc4db863096f4ec
gary909 <garywhite61@hotmail.com>
2023-03-06 12:46:14 +0100

    code working

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 function correctPolishLetters (string) {
-    // your code
+    let myName = "";
+    for(let i = 0; i < string.length; i++){
+        if(string[i] == 'ą'){
+            myName += 'a';
+        } else if(string[i] == 'ć'){
+            myName += 'c';
+        } else if(string[i] == 'ę'){
+            myName = myName += 'e';
+        } else if(string[i] == 'ł'){
+            myName += 'l';
+        } else if(string[i] == 'ń'){
+            myName += 'n';
+        } else if(string[i] == 'ó'){
+            myName += 'o';
+        } else if(string[i] == 'ś'){
+            myName += 's';
+        } else if(string[i] == 'ź'){
+            myName += 'z';
+        } else if(string[i] == 'ż'){
+            myName += 'z';
+        } else {
+            myName += string[i];
+        }
+    }
+    return myName;
 }
 
 console.log(correctPolishLetters("Jędrzej Błądziński")); // "Jedrzej Bladzinski"
