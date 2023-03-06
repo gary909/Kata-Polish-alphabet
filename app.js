39e821c70c5d04f766661cc31828084c487b621e
gary909 <garywhite61@hotmail.com>
2023-03-06 12:33:09 +0100

    code

---
+++ app.js
@@ -0,0 +1,7 @@
+function correctPolishLetters (string) {
+    // your code
+}
+
+console.log(correctPolishLetters("Jędrzej Błądziński")); // "Jedrzej Bladzinski"
+console.log(correctPolishLetters("Lech Wałęsa")); // "Lech Walesa"
+console.log(correctPolishLetters("Maria Skłodowska-Curie")); // "Maria Sklodowska-Curie"
